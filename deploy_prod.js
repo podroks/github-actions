78aad2b2400660464dfd824d6893e665b9b2d8a0
florian <bouilleteauflorian@outlook.fr>
2022-03-31 00:12:58 +0200

    update scripts

--- deploy_prod.js
+++ deploy_prod.js
@@ -1,5 +1,7 @@
-module.exports = async ({ github, branch, core }) => {
+module.exports = async ({ github, branch, context, core }) => {
     console.log(Object.keys(github));
+    console.log(Object.keys(context));
+    console.log(branch);
     if (!branch || !branch.includes("master")) {
       console.log("Attend, mais tu n'est pas sur master ! Pas contant");
   
